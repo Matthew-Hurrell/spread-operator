d2801857240a41398bf0dfebbc17a4270c20bfc6
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-11-14 17:18:09 +0000

    Initial commit

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -5,8 +5,27 @@
 
 // No spread operator
 
-// Copying an array
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log(arr2);
+console.log(arr1);
+// Copying an array into a new variable without altering the original
+let arr3 = [1, 2, 3];
+let arr4 = [...arr3];
+
+arr4.push(7);
+console.log(arr4);
+console.log(arr3);
 
 // Copying an object
+let obj1 = { a: 1, b: 2, c: 3 };
+let obj2 = { ...obj1, d: 4 };
+let obj3 = { ...obj1, b: 5};
+console.log(obj2);
+console.log(obj1);
+console.log(obj3);
 
 // Copying only part of an array/object
+let arr5 = [ ...arr1, { ...obj1 }, ...arr3, "x", "y", "z"];
+console.log(arr5);
